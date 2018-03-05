03d316e4f8f066e75ea6ff4d0a8437cd96678046
Henrique Campos <henriiquecampos@gmail.com>
2018-03-05 15:52:26 -0300

    Fixed #6 (#7)
    Seems to be exactly as though, remember to not use Godot bultin `ui_actions` for anything that is not an UI interaction

--- actors/player/player_character.gd
+++ actors/player/player_character.gd
@@ -2,14 +2,14 @@ extends "res://actors/basic_character.gd"
 func _physics_process(delta):
 	if !can_move:
 		return
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_just_pressed("jump"):
 		apply_jump()
-	if Input.is_action_just_released("ui_up"):
+	if Input.is_action_just_released("jump"):
 		cancel_jump()
 		
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("walk_right"):
 		walk(1)
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("walk_left"):
 		walk(-1)
 	else:
 		walk(0)
