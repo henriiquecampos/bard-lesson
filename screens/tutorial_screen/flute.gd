4af42ab1ef7c26b1833026679283e7ea2277b942
Henrique Campos <henriiquecampos@gmail.com>
2018-03-06 15:59:38 -0300

    Changed tutorial to behave like a key binding setting
    Players now can choose which keys will be used for playing the flute.
    
    Will, from bass to tremble, be the first three keys pressed by the player.
    
    Still has to fix bard.gd behavior

--- screens/tutorial_screen/flute.gd
+++ screens/tutorial_screen/flute.gd
@@ -2,25 +2,31 @@ extends Sprite
 const PITCHES = [1.0, 1.25, 1.50]
 
 func _ready():
-	set_process(false)
-	
-func _process(delta):
-	$Keys/Z.set_pressed(Input.is_key_pressed(KEY_Z))
-	$Keys/X.set_pressed(Input.is_key_pressed(KEY_X))
-	$Keys/C.set_pressed(Input.is_key_pressed(KEY_C))
-	
-	if Input.is_action_just_pressed("interact"):
-		if Input.is_key_pressed(KEY_Z):
-			play_flute(0)
-		elif Input.is_key_pressed(KEY_X):
-			play_flute(1)
-		elif Input.is_key_pressed(KEY_C):
-			play_flute(2)
-	if Input.is_action_just_released("interact"):
-		$SFX.stop()
-	
+	set_process_input(false)
+
 func play_flute(pitch):
 	var bus = AudioServer.get_bus_index($SFX.get_bus())
 	var fx = AudioServer.get_bus_effect(bus, 0)
 	fx.set_pitch_scale(PITCHES[pitch])
 	$SFX.play()
+	
+func _input(event):
+	if (event is InputEventKey):
+		var p = 0
+		var k = ""
+		print(InputMap.get_action_list("interact").size())
+		if PITCHES.size() > InputMap.get_action_list("interact").size():
+			InputMap.action_add_event("interact", event)
+
+		for e in InputMap.get_action_list("interact"):
+			if e.as_text() == event.as_text():
+				p = (InputMap.get_action_list("interact").find(e))
+				k = e.as_text().to_lower()
+
+		if event.is_action_pressed("interact"):
+			play_flute(p)
+			$Keys.get_child(p).set_pressed(true)
+			$Keys.get_child(p).get_node("Letter").set_text(k)
+		elif event.is_action_released("interact"):
+			$SFX.stop()
+			$Keys.get_child(p).set_pressed(false)
