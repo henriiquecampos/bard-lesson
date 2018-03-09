65de90a441bdcd371fc91f7501323c764536da4f
Henrique Campos <henriiquecampos@gmail.com>
2018-03-09 17:16:04 -0300

    Fixed tutorial not clearing default keys

--- screens/tutorial_screen/flute.gd
+++ screens/tutorial_screen/flute.gd
@@ -3,6 +3,10 @@ const PITCHES = [1.0, 1.25, 1.50]
 
 func _ready():
 	set_process_input(false)
+	
+func clear_keys():
+	for e in InputMap.get_action_list("interact"):
+		InputMap.action_erase_event("interact", e)
 
 func play_flute(pitch):
 	var bus = AudioServer.get_bus_index($SFX.get_bus())
@@ -14,7 +18,6 @@ func _input(event):
 	if (event is InputEventKey):
 		var p = 0
 		var k = ""
-		print(InputMap.get_action_list("interact").size())
 		if PITCHES.size() > InputMap.get_action_list("interact").size():
 			InputMap.action_add_event("interact", event)
 
